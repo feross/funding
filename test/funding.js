fbb1fb862e4a757c935b8072530ae95b094342c2
Feross Aboukhadijeh <feross@feross.org>
2019-08-19 22:22:45 -0700

    fix tests for node 6

--- test/funding.js
+++ test/funding.js
@@ -1,18 +1,16 @@
 const test = require('tape')
 const cp = require('child_process')
 const path = require('path')
-const util = require('util')
-
-const execFile = util.promisify(cp.execFile)
 
 const FUNDING_BIN_PATH = path.join(__dirname, '..', 'bin', 'funding.js')
 
-test('Santiy check bin/funding.js output', async t => {
-  const { stdout, stderr } = await execFile(FUNDING_BIN_PATH)
+test('Santiy check bin/funding.js output', t => {
+  t.plan(4)
 
-  t.ok(stdout.length > 0, 'there exists some stdout ouput')
-  t.ok(!stdout.match(/error/gi), 'stdout output is not an error')
-  t.equal(stderr.length, 0, 'no stderr output')
-
-  t.end()
+  cp.execFile(FUNDING_BIN_PATH, (err, stdout, stderr) => {
+    t.error(err)
+    t.ok(stdout.length > 0, 'there exists some stdout ouput')
+    t.ok(!stdout.match(/error/gi), 'stdout output is not an error')
+    t.equal(stderr.length, 0, 'no stderr output')
+  })
 })
