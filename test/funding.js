b24ccba2bb32eaaa67c2066ec279a67683ac563e
Feross Aboukhadijeh <feross@feross.org>
2019-08-23 18:17:57 -0700

    test: Fix tests on Node 6

--- test/funding.js
+++ test/funding.js
@@ -11,7 +11,7 @@ test('Sanity check bin/funding.js output', t => {
     t.error(err)
     t.ok(stdout.length > 0, 'there exists some stdout ouput')
     t.ok(!stdout.match(/error/gi), 'stdout output is not an error')
-    t.equal(stderr.length, 0, 'no stderr output')
+    t.equal(stderr, '', 'no stderr output')
   })
 })
 
@@ -19,16 +19,15 @@ test('`npm --silent` or `npm --loglevel silent` prevents output', t => {
   t.plan(3)
 
   const opts = {
-    env: {
-      ...process.env,
+    env: Object.assign({}, process.env, {
       npm_config_loglevel: 'silent'
-    }
+    })
   }
 
   cp.execFile(FUNDING_BIN_PATH, [], opts, (err, stdout, stderr) => {
     t.error(err)
-    t.equal(stdout.length, 0, 'no stdout ouput')
-    t.equal(stderr.length, 0, 'no stderr output')
+    t.equal(stdout, '', 'no stdout ouput')
+    t.equal(stderr, '', 'no stderr output')
   })
 })
 
@@ -36,16 +35,15 @@ test('`npm --quiet` or `npm --loglevel warn` prevents output', t => {
   t.plan(3)
 
   const opts = {
-    env: {
-      ...process.env,
+    env: Object.assign({}, process.env, {
       npm_config_loglevel: 'warn'
-    }
+    })
   }
 
   cp.execFile(FUNDING_BIN_PATH, [], opts, (err, stdout, stderr) => {
     t.error(err)
-    t.equal(stdout.length, 0, 'no stdout ouput')
-    t.equal(stderr.length, 0, 'no stderr output')
+    t.equal(stdout, '', 'no stdout ouput')
+    t.equal(stderr, '', 'no stderr output')
   })
 })
 
@@ -53,15 +51,14 @@ test('`npm --loglevel error` prevents output', t => {
   t.plan(3)
 
   const opts = {
-    env: {
-      ...process.env,
+    env: Object.assign({}, process.env, {
       npm_config_loglevel: 'error'
-    }
+    })
   }
 
   cp.execFile(FUNDING_BIN_PATH, [], opts, (err, stdout, stderr) => {
     t.error(err)
-    t.equal(stdout.length, 0, 'no stdout ouput')
-    t.equal(stderr.length, 0, 'no stderr output')
+    t.equal(stdout, '', 'no stdout ouput')
+    t.equal(stderr, '', 'no stderr output')
   })
 })
